ceff39f6ebab2963ea2d548c332f7b8b817bdd0d
xl-9527 <1824670705@qq.com>
2025-03-03 07:22:22 +0800

    feat(users): 更新用户注册功能- 修改注册页面表单，添加 CSRF 令牌并修正提交路径- 新增用户注册的 POST 请求处理方法 - 添加日志记录器引用，准备记录注册信息

--- django-project/apps/users/views.py
+++ django-project/apps/users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+import logging
+
+log = logging.getLogger('django')
 
 # Create your views here.
 class RegistryView(View):
     def get(self, request):
         return render(request, 'users/registry.html')
-    pass
+
+    def post(self, request):
+        # log.info(request)
+        pass
