d5edf762af990e4f0eb2fa1e5798c53fddd4a118
xl-9527 <1824670705@qq.com>
2025-03-03 10:22:51 +0800

    处理用户注册的请求

--- django-project/apps/users/views.py
+++ django-project/apps/users/views.py
@@ -1,14 +1,27 @@
+import logging
+
 from django.shortcuts import render
 from django.views import View
-import logging
+from models import DjangoUser
 
 log = logging.getLogger('django')
 
+
 # Create your views here.
 class RegistryView(View):
+    """用户注册"""
+
     def get(self, request):
+        """访问用户祖册的页面"""
         return render(request, 'users/registry.html')
 
     def post(self, request):
+        """处理用户注册的请求"""
         # log.info(request)
-        pass
+        post = request.POST
+        log.info("用户名：" + post.get('username'))
+        log.info('密码：' + post.get('password'))
+
+        # DjangoUser(username=post.get('username'), password=post.get('password')).save()
+
+        return render(request, 'index.html')
